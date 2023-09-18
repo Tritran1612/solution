99e19516b18a30fde7f2015676f0bbc2f4593e2f
Tritran1612 <minhtri16122004@gmail.com>
2023-09-18 21:32:59 +0700

    khong co gi

---
+++ solutions/lambaitap_hutech.c
@@ -0,0 +1,85 @@
+#include <stdio.h>
+// nhập các phần từ ma trận
+//int main() {
+//    // Khai báo và khởi tạo ma trận 2x2
+//    int matrix[2 ][6 ] = {
+//            {1, 2,4,5,6,7},
+//            {3, 4,2,4,6,7}
+//    };
+//
+//    // Truy cập và in các phần tử trong ma trận
+//    printf("Ma tran:\n");
+//    for (int i = 0; i < 2; i++) {
+//        for (int j = 0; j < 6; j++) {
+//            printf("%d ", matrix[i][j]);
+//        }
+//        printf("\n");
+//    }
+//
+//    return 0;
+//}
+
+//TRUNG BÌNH CỘNG CÁC PHẦN TỬ
+//int main(){
+//    int n;
+//    printf("nhap so luong phan tu:");
+//    scanf("%d",&n);
+//    int a[n];
+//    for(int i=0;i<n;i++){
+//        printf("a[%d]:",i);
+//        scanf("%d",&a[i]);
+//    }
+//    for(int i=0;i<n;i++){
+//        printf("%d",a[i]);
+//    }
+//    int sum=0;
+//    for(int i=0;i<n;i++){
+//        sum+=a[i];
+//    }
+//}
+
+// tạo hình tam giác
+
+//int main(){
+//    int n;
+//    printf("nhap n:");
+//    scanf("%d",&n);
+//    for(int i=0;i<n;i++){
+//        for(int j=0;j<n;j++){
+//            if(i>j){
+//                printf("*");
+//            }
+//
+//
+//        }
+//        printf("\n");
+//    }
+//}
+
+// tim vi tri xuat hien cuoi cung cua phan tu có giá trị x nếu có trong dãy b
+int timkihieu(int a[],int n,int ketqua){
+    for(int i=0;i<n;i++){
+        if(a[i]==ketqua){
+            return 1;
+        }
+        return 0;
+    }
+}
+int timketqua(int a[],int n,int ketqua){
+    int cc= timkihieu(a,n,ketqua);
+    for(int i=0;i<n;i++){
+        if(cc){
+            printf("co so %d trong day ", ketqua);
+        }
+    }
+}
+int main(){
+    int n;
+    printf("nhap so luong:");
+    scanf("%d",&n);
+    int a[n];
+    int ketqua;
+    printf("nhap so can xet:");
+    scanf("%d", &ketqua);
+    timketqua(a,n,ketqua);
+}
